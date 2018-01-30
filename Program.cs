b236b5aac70f8fec6f3a5b5efe054eae6ffbd133
narumjungsnagin <joonst26@gmail.com>
2018-01-30 08:50:56 -0600

    Already?

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if(year % 4 == 0)
+                Console.WriteLine("It is leap year");
+            
+            if(year % 4 != 0)
+                Console.WriteLine("It is Not Leap year");
+        }
+    }
+}
